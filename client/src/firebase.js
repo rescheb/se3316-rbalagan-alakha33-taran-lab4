5184bedae6b05b9d08a1d8d20ad6cee2ab452c1a
Thilakshanan Aran <thilakshananaran@gmail.com>
2022-11-30 04:07:25 -0500

    SignUp Page, Authentication,

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/compat/app'
 import "firebase/compat/auth"
  
 const app = firebase.initializeApp({
- apiKey: process.env.REACT_APP_FIREBASE_API_KEY ,
+ apiKey: "AIzaSyDC6W9rFe1FO0I_TNzYiwE28xqHIjTgwaY" ,
  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
