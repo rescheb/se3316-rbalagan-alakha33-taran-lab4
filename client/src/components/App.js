9ffa40d7b17fc3beefd0e1fcbba106949ae1d993
resche-b <rbalagan@uwo.ca>
2022-11-30 05:30:14 -0500

    playlist titles visible

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,14 +3,17 @@ import Signup from "./SignUp"
 import {Container} from 'react-bootstrap'
 import { AuthProvider } from "../contexts/AuthContext"
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
  
  
 function App(){
 
+const [listOfPlaylists, setListOfPlaylists] = useState([]);
+
 useEffect(() => {
 
   axios.get("http://localhost:9000/playlist").then((response) =>{
+    setListOfPlaylists(response.data)
     console.log(response.data);
 
   })
@@ -19,8 +22,15 @@ useEffect(() => {
 
  return (
 
- <AuthProvider>
- <Container className = "d-flex align-items-center justify-content-center"
+  <div>
+    <div className="Playlists">
+      {listOfPlaylists.map((value, key) => {
+        return <div>{value.title}</div>
+        })}
+    </div>
+
+  <AuthProvider>
+    <Container className = "d-flex align-items-center justify-content-center"
    style = {{minHeight: "100vh"}}
    >
      <div className = "w-100" style = {{maxWidth: "500px"}}>
@@ -28,6 +38,11 @@ useEffect(() => {
  </div>
  </Container>
  </AuthProvider>
+
+  </div>
+
+ 
+ 
  )
 }
  
