8fd9b0d4c19177ac174fe05fcf7c93e2dc560c9b
resche-b <rbalagan@uwo.ca>
2022-11-30 09:34:53 -0500

    playlist function

--- client/src/components/Playlist.js
+++ client/src/components/Playlist.js
@@ -3,44 +3,61 @@ import { useEffect, useState } from "react";
 
 
 
+
 export default function Playlist()
 {
+let data=[];
 const [listOfPlaylists, setListOfPlaylists] = useState([]);
 
 useEffect(() => {
 
   axios.get("http://localhost:9000/playlist").then((response) =>{
     setListOfPlaylists(response.data)
-    console.log(response.data);
-    console.log(listOfPlaylists);
-
+    
+   
+    
   })
+
 
 }, [])
 
+data = listOfPlaylists;
 
-let recentPlaylists = [[]];
-let h=1;
+let recentPlaylists = [];
 let l;
 
-while(h<=10)
+while(recentPlaylists.length<10)
 {
-    l=response.data[0];
-    for(let x=0; x<response.data.length;x++)
+    l=data[0];
+    for(let x=0; x<data.length;x++)
     {
         
-        if(new Date(l.updatedAt) < new Date(response.data[x]))
+        if(new Date(l.updatedAt.split(" ")[0]) < new Date(data[x].updatedAt.split(" ")[0]))
         {
-            l=response.data[x];
+            l=data[x];
         }
-        else if(+(new Date(l.updatedAt)) === +(new Date(response.data[x])))
+
+        else if(+(new Date(l.updatedAt.split(" ")[0])) === +(new Date(data[x].updatedAt.split(" ")[0])))
         {
-            
+            if(l.updatedAt.split(" ")[1]<data[x].updatedAt.split(" ")[1])
+            {
+                l=data[x];
+            }
         }
-    
     }
+
+    // for(let p=0;p<data.length;p++)
+    // {
+    //     if(l.username===data[p].username && l.title === data[p].title)
+    //     {
+    //         data.remove(data[p]);
+    //     }
+    // }
+    recentPlaylists.push(l);
+
 }
 
+console.log(recentPlaylists);
 
 return(
 <div className="Playlists">
