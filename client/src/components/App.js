4efd5a7a8172f66623c1411b0dd0678b700f6316
resche-b <rbalagan@uwo.ca>
2022-11-30 05:10:11 -0500

    getting sql in frontend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,10 +2,23 @@
 import Signup from "./SignUp"
 import {Container} from 'react-bootstrap'
 import { AuthProvider } from "../contexts/AuthContext"
+import axios from "axios";
+import { useEffect } from "react";
  
  
 function App(){
+
+useEffect(() => {
+
+  axios.get("http://localhost:9000/playlist").then((response) =>{
+    console.log(response.data);
+
+  })
+
+}, [])
+
  return (
+
  <AuthProvider>
  <Container className = "d-flex align-items-center justify-content-center"
    style = {{minHeight: "100vh"}}
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 app.use(express.json())
+app.use(cors());
 
 const db = require('./models')
 
@@ -9,8 +12,8 @@ const playlistRouter = require ("./routes/playlist");
 app.use("/playlist", playlistRouter);
 
 db.sequelize.sync().then(()=> {
-    app.listen(3000, () => {
-        console.log("Server is running on port 3000");
+    app.listen(9000, () => {
+        console.log("Server is running on port 9000");
     });
 })
 
