7937858ec9b4b546032452e5b8f3baef3dac9e3b
Thilakshanan Aran <thilakshananaran@gmail.com>
2022-12-01 06:58:05 -0500

    Error Checks2

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -6,6 +6,7 @@ import {Link , useHistory} from 'react-router-dom'
 export default function Login() {
    const emailRef = useRef()
    const passwordRef = useRef()
+
    const {login} = useAuth()
    const [error,setError] = useState('')
    const [loading, setLoading] = useState(false)
