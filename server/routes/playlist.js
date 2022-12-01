2d6369eca35398ad34106f4eedcb9dcdd7ed0999
Thilakshanan Aran <thilakshananaran@gmail.com>
2022-12-01 04:13:12 -0500

    Edited Login and Database

--- server/routes/playlist.js
+++ server/routes/playlist.js
@@ -16,4 +16,18 @@ router.post('/',async (req,res)=> {
     res.json(play);
 });
 
+
+router.get('/recentPlaylists',(req,res) => {
+    db.query('SELECT * FROM playlists ORDER BY updatedAt DESC LIMIT 10', (err, data) => {
+        console.log(data);
+        if(err != null){
+           res.json(err)
+        }
+        else{
+            res.json(data)
+        }})
+    }
+
+);
+
 module.exports = router;
