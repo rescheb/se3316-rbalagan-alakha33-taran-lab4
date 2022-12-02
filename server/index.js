eb67b323caf9094f464cd052edce2268467f9a95
resche-b <rbalagan@uwo.ca>
2022-12-02 04:12:34 -0500

    private playlist update

--- server/index.js
+++ server/index.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const sql = require('mysql2');
 
 app.use(express.json())
 app.use(cors());
 
-const db = require('./models')
+const db = sql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'musicdb'
+});
+
+db.query("CREATE TABLE playlists (id INT NOT NULL AUTO_INCREMENT,title VARCHAR(45) NOT NULL,song VARCHAR(45) NOT NULL,username VARCHAR(45) NOT NULL,ispublic BIT NOT NULL,createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY (id));", (req,res) => {
+
+});
 
 //Routers
 const playlistRouter = require ("./routes/playlist");
 app.use("/playlist", playlistRouter);
 
-db.sequelize.sync().then(()=> {
+
     app.listen(9000, () => {
         console.log("Server is running on port 9000");
     });
-})
+
 
 
 
