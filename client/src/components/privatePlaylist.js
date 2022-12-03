bf37ab879d2ca182f0c6b68751f2eb54bac6052b
resche-b <rbalagan@uwo.ca>
2022-12-03 03:45:31 -0500

    update to playlist

--- client/src/components/privatePlaylist.js
+++ client/src/components/privatePlaylist.js
@@ -9,7 +9,7 @@ export default function PrivatePlaylist()
 const createPlaylist = () => {
 
     console.log("Entered")
-    fetch("http://" + window.location.hostname + ':9000/playlist/playlist', {method: "POST", body: JSON.stringify({"title": title, "songs": song, "username": "test2", "public": 1}), headers: new Headers({'Content-Type': 'application/json'})})
+    fetch("http://" + window.location.hostname + ':9000/playlist/playlist', {method: "POST", body: JSON.stringify({"title": title, "songs": song, "username": "test2", "public": ispublic}), headers: new Headers({'Content-Type': 'application/json'})})
     .then(res => res.json())
     .then(data => {
         
@@ -34,7 +34,7 @@ const handleChange = () => {
         <div className = "createPlaylist" >
         <button onClick={createPlaylist} className="cpbtn">Create Playlist</button>
         <input type="text" placeholder="Enter Playlist Name" required value={title} onChange={(e) => setTitle(e.target.value)}/>  
-        <input type="checkbox" required checked={ispublic} onChange={(e) => setIsPublic(e.target.value)}/>ispublic
+        <input type="checkbox" required checked={ispublic} onChange={(e) => setIsPublic(e.target.checked)}/>ispublic
         <br></br>
         <input type="text" placeholder="Enter Songs" required value={song} onChange={(e) => setSong(e.target.value)}/>
         </div>
