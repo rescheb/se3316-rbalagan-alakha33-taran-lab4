80de8bf0438cb7d3716a67910bda93bb222eaeaf
aarishlakhani <alakha33@uwo.ca>
2022-12-04 04:32:01 -0500

    styling and adjusting the frontend

--- client/src/components/Playlist.js
+++ client/src/components/Playlist.js
@@ -1,52 +1,54 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Search from "./Search";
-
-
-
+import "./App.css";
 
 export default function Playlist() {
+  const [recentPlaylists, setRecentPlaylists] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:9000/playlist/recentPlaylists", {
+      method: "GET",
+      headers: new Headers({ "Content-Type": "application/json" }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setRecentPlaylists(data);
+        console.log(JSON.stringify(data));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-
-    const [recentPlaylists, setRecentPlaylists] = useState([]);
-    useEffect(() => {
-        fetch("http://localhost:9000/playlist/recentPlaylists", { method: "GET", headers: new Headers({ 'Content-Type': 'application/json' }) })
-            .then(res => res.json())
-            .then(data => {
-                setRecentPlaylists(data);
-                console.log(JSON.stringify(data));
-            })
-            .catch(err => { console.log(err) })
-    }, []);
-
-    return (
-        <div >
-
-            {recentPlaylists.length != 0 ? recentPlaylists.map(playlist =>
-
-                <div>
-                    <ul>
-                        <li> {playlist.title + " by: " + playlist.username + " #tracks: " + playlist.song.split(",").length} </li>
-                        {/* <li> {playlist.title} </li>
+  return (
+    <div>
+      <h1>Public Playlist</h1>
+      <div className="playlistSearch">
+        <Search />
+      </div>
+      {recentPlaylists.length != 0
+        ? recentPlaylists.map((playlist) => (
+            <div>
+              <ul>
+                <li>
+                  {" "}
+                  {playlist.title +
+                    " by: " +
+                    playlist.username +
+                    " #tracks: " +
+                    playlist.song.split(",").length}{" "}
+                </li>
+                {/* <li> {playlist.title} </li>
                         <li> {playlist.song} </li>
                         <li> {playlist.username} </li>
                         <li> {playlist.ispublic} </li>
                         <li> {playlist.createdAt} </li>
                         <li> {playlist.updatedAt} </li> */}
-                    </ul>
-                    <br></br>
-
-                </div>
-            ) : null}
-
-
-            <Search/>
-
-
-
-
-        </div>
-    );
-
-
+              </ul>
+              <br></br>
+            </div>
+          ))
+        : null}
+    </div>
+  );
 }
